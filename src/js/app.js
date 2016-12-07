61fd13206c69d357efa930aa187d0233c62ad2de
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2016-12-07 16:02:14 +0200

    add mq/responsive

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,15 @@
 //=include common.js
 
 $(document).ready(function() {
-
+	//nav
+	$('.js-nav-btn').on('click', function() {
+		$('.js-nav').toggleClass('is-active');
+		return false;
+	});
+	$('body').on('click', function() {
+		$('.js-nav').removeClass('is-acitve');
+	});
+	$('.js-nav').on('click', function(event) {
+		event.stopPropagation();
+	});
 });
